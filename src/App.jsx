266dbd33932e103ed35aa68160f7fa4e7c5fee69
Aditya Chandra <aditya.anil.chandra@gmail.com>
2025-01-04 15:53:49 +0530

    Navbar and dynamic title done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Hero from './components/Hero'
-import About from './components/about'
+import About from './components/About'
 import Navbar from './components/Navbar'
 
 const App = () => {
